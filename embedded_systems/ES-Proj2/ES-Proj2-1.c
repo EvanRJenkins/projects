31e9739fd274b46256c9022b4ee2fcc5daff9288
Evan Jenkins <JenkinsE@jbu.edu>
2025-09-06 08:40:48 -0500

    Update ES-Proj2-1.c

--- embedded_systems/ES-Proj2/ES-Proj2-1.c
+++ embedded_systems/ES-Proj2/ES-Proj2-1.c
@@ -1,65 +1,63 @@
 #include <msp430.h>
 #define LED1 BIT0
-#define LED2 BIT6
 #define DELAYLOOPS 10000
 
 #define STATE_IDLE 0
 #define STATE_ON 1
 #define STATE_OFF 2
 
-
-void main(void)
-{
-volatile unsigned int LoopCounter;                                // "volatile" ensures will not be optimized away
-unsigned int currentState = STATE_IDLE;
-WDTCTL = WDTPW + WDTHOLD;                                         // Stop watchdog timer
-                                                                  // Set 
-P1DIR = LED1;                                                     // Set P1.0 (red LED) to output and P1.3 (button) to input
-P1OUT = ~LED1;                                                    // start with LED1 off, LED2 on
-P1REN |= BIT3;                                                    // enable input resistor
-P1OUT |= BIT3;                                                    // set pull-up resistor
-while (1)                                                         // loop forever
-{
-
-  switch (currentState)   // Finite state machine
+void main(void) {
+  volatile unsigned int
+      LoopCounter; // "volatile" ensures will not be optimized away
+  unsigned int currentState = STATE_IDLE;
+  WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+                            // Set
+  P1DIR = LED1;  // Set P1.0 (red LED) to output and P1.3 (button) to input
+  P1OUT = ~LED1; // start with LED1 off, LED2 on
+  P1REN |= BIT3; // enable input resistor
+  P1OUT |= BIT3; // set pull-up resistor
+  while (1)      // loop forever
   {
+
+    switch (currentState) // Finite state machine
+    {
     case STATE_IDLE:
-      if (!(P1IN & BIT3))             // If button rising edge
+      if (!(P1IN & BIT3)) // If button rising edge
       {
-        P1OUT |= BIT0;               // Set LED
-        currentState = STATE_ON;          // Go to on state
-      }
-      else currentState = STATE_IDLE;     // Else stay
+        P1OUT |= BIT0;           // Set LED
+        currentState = STATE_ON; // Go to on state
+      } else
+        currentState = STATE_IDLE; // Else stay
       break;
 
     case STATE_ON:
-      if (P1IN & BIT3)                // If button falling edge
-      {  
-        for (LoopCounter = 0; LoopCounter < DELAYLOOPS; ++LoopCounter)    // Wait for DELAY_LOOPS cycles
+      if (P1IN & BIT3) // If button falling edge
+      {
+        for (LoopCounter = 0; LoopCounter < DELAYLOOPS;
+             ++LoopCounter) // Wait for DELAY_LOOPS cycles
         {
           ;
         }
-        if (!(P1IN & BIT3))                 // If button rising edge
+        if (!(P1IN & BIT3)) // If button rising edge
         {
-          P1OUT &= ~BIT0;                   // Reset LED
-          currentState = STATE_OFF;         // Go to off state
-        }
-        else currentState = STATE_ON;       // Else stay
+          P1OUT &= ~BIT0;           // Reset LED
+          currentState = STATE_OFF; // Go to off state
+        } else
+          currentState = STATE_ON; // Else stay
       }
       break;
-
-
 
     case STATE_OFF:
-    if (P1IN & BIT3)                // If button falling edge
-    {
-      for (LoopCounter = 0; LoopCounter < DELAYLOOPS; ++LoopCounter)    // Wait for DELAY_LOOPS cycles
+      if (P1IN & BIT3) // If button falling edge
       {
-        ;
+        for (LoopCounter = 0; LoopCounter < DELAYLOOPS;
+             ++LoopCounter) // Wait for DELAY_LOOPS cycles
+        {
+          ;
+        }
+        currentState = STATE_IDLE; // Go back to idle
+        break;
       }
-      currentState = STATE_IDLE;   // Go back to idle
-      break;
     }
   }
-}
 }
