4c0d98eac3fd69352bbc8dd13430fe0d1f1b1e1c
Evan Jenkins <JenkinsE@jbu.edu>
2025-09-21 16:34:53 -0500

    Refactored to use TIMER_A1 vector instead of watchdog, removed unecessary reset state

--- embedded_systems/ES-Proj5/ES-Proj5-3.c
+++ embedded_systems/ES-Proj5/ES-Proj5-3.c
@@ -1,83 +1,94 @@
 #include <msp430g2553.h>
 
-typedef enum { PROGRAM_1, PROGRAM_2, PROGRAM_3, RESET_PROGRAM } program_t;
+typedef enum { PROGRAM_1, PROGRAM_2, PROGRAM_3 } program_t;
 
 volatile program_t current_program = PROGRAM_1;
-volatile unsigned int current_program_time =
-    40; // 40 * 250 ms = approx. 10 seconds
+volatile unsigned int current_program_time = 40; // 40 * 250 ms = abt 10 seconds
 
-void blink_indicator(unsigned char count);
+void program_transition_blink(unsigned char num_blinks);
+
+#define TACCR1_CCIFG 0x02
 
 void main(void) {
-  WDTCTL = WDTPW + WDTHOLD; // Stop watchdog timer
+    WDTCTL = WDTPW + WDTHOLD;   // Stop watchdog timer
+    
+    // Clock setup
+    BCSCTL3 |= LFXT1S_2;        // ACLK = VLO (12kHz)
+    
+    // GPIO setup
+    P1DIR |= (BIT0 | BIT6);     // Init LEDs to output
+    P1OUT &= ~(BIT0 | BIT6);    // Init LEDs off
+    
+    // Timer_A setup
+    TACTL = TASSEL_1 + MC_2 + TACLR;  // ACLK, Continuous Mode, Clear timer
+    TACCR0 = 6000;              // 500 ms period for LED blink
+    TACCR1 = 3000;              // 250 ms period for program transition
+    TACCTL1 = CCIE;             // Enable CCR1 interrupt
+    
+    __enable_interrupt();       // Enable global interrupts
 
-  BCSCTL3 |= LFXT1S_2; // ACLK = VLO
+    while (1) {
+        switch (current_program) {
+            case PROGRAM_1:
+                program_transition_blink(1);
+                TACCTL0 &= ~CCIE;   // Disable CCR0 interrupt
+                P1OUT &= ~BIT6;     // Confirm green LED is off
+                while (current_program == PROGRAM_1) {
+                    P1OUT ^= BIT6;  // Toggle green LED
+                    __delay_cycles(500000); // Active mode delay
+                }
+                break;
 
-  P1DIR |= (BIT0 | BIT6); // Set LEDs to output direction
-  P1OUT &= ~(BIT0 | BIT6);
+            case PROGRAM_2:
+                program_transition_blink(2);
+                TACCTL0 |= CCIE;    // Enable CCR0 interrupt
+                while (current_program == PROGRAM_2) {
+                    __low_power_mode_0(); // Enter LPM0
+                }
+                break;
 
-  WDTCTL = WDT_ADLY_250; // Set WDT interval to ~250ms from ACLK
-  IE1 |= WDTIE;          // Enable WDT interrupt
-
-  __enable_interrupt(); // Enable global interrupts
-
-  while (1) {
-    switch (current_program) {
-    case PROGRAM_1:
-      blink_indicator(1);
-      TACTL = TASSEL_2 + TACLR; // Pause timer A
-      while (current_program == 1) {
-        P1OUT ^= BIT6;
-        __delay_cycles(500000); // Blink delay active
-      }
-      break;
-
-    case PROGRAM_2:
-      blink_indicator(2);
-      // Configure Timer_A with SMCLK (1 MHz) and /8 divider
-      TACTL =
-          TASSEL_2 + MC_1 + TACLR + ID_3; // SMCLK, Up Mode, Clear, /8 divider
-      TACCR0 = 62500;                     // 500ms period for blinking
-      TACCTL0 = CCIE;                     // Enable CCR0 interrupt
-      __low_power_mode_0();               // Enter LPM0
-      break;
-
-    case PROGRAM_3:
-      blink_indicator(3);
-      // Configure Timer_A with ACLK (12 kHz)
-      TACTL = TASSEL_1 + MC_1 + TACLR; // ACLK, Up Mode, Clear
-      TACCR0 = 6000;                   // 500ms period for blinking
-      TACCTL0 = CCIE;                  // Enable CCR0 interrupt
-      __low_power_mode_3();            // Enter LPM3
-      break;
+            case PROGRAM_3:
+                program_transition_blink(3);
+                TACCTL0 |= CCIE;    // Enable CCR0 interrupt
+                while (current_program == PROGRAM_3) {
+                    __low_power_mode_3(); // Enter LPM3
+                }
+                break;
+        }
     }
-  }
 }
 
-void blink_indicator(unsigned char count) {
-  unsigned char i;
-  for (i = 0; i < count; i++) {
-    P1OUT |= BIT0;
-    __delay_cycles(100000); // 100 ms on delay
-    P1OUT &= ~BIT0;
-    __delay_cycles(100000); // 100 ms off delay
-  }
-}
-
-#pragma vector = WDT_VECTOR
-__interrupt void WDT_ISR(void) {
-  current_program_time--;
-  if (current_program_time == 0) {
-    current_program_time = 40; // Reset
-    current_program++;
-    if (current_program > PROGRAM_3) {
-      current_program = PROGRAM_1;
+void program_transition_blink(unsigned char num_blinks) {
+    unsigned char i;
+    for (i = 0; i < num_blinks; ++i) {
+        P1OUT |= BIT0;          // Turn on red LED
+        __delay_cycles(100000); // ~100ms on delay
+        P1OUT &= ~BIT0;         // Turn off red LED
+        __delay_cycles(100000); // 100ms off delay
     }
-    _low_power_mode_off_on_exit(); // Wake up CPU
-  }
+    __delay_cycles(200000);
 }
 
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void Timer_A0_ISR(void) {
-  P1OUT ^= BIT6; // Toggle green LED
+    P1OUT ^= BIT6;              // Toggle green LED
+    TACCR0 += 6000;             // Add offset for next interrupt
+}
+
+#pragma vector = TIMER0_A1_VECTOR
+__interrupt void Timer_A1_ISR(void) {
+    if (TAIV == TACCR1_CCIFG) {  // Check interrupt vector
+
+        TACCR1 += 3000;     // Add offset for next interrupt
+        --current_program_time;
+        if (current_program_time == 0) {
+            current_program_time = 40;  // Reset to 10 s
+            ++current_program;
+            if (current_program > PROGRAM_3) {
+                current_program = PROGRAM_1;
+            }
+            __low_power_mode_off_on_exit(); // Wake up
+        }
+
+    }
 }
