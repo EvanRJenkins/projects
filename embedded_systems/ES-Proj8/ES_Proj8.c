6dd926aa66f4ff5d3d7ae9417d7c1cdb4b531389
Evan Jenkins <JenkinsE@jbu.edu>
2025-10-12 15:26:40 -0500

    Update ES_Proj8.c

--- embedded_systems/ES-Proj8/ES_Proj8.c
+++ embedded_systems/ES-Proj8/ES_Proj8.c
@@ -1,72 +1,55 @@
 #include <msp430.h>
 
-#define WRITE_ADDR  0x50     // Device address for a write operation
-#define READ_ADDR 0x51      // Device address for a read operation
+#define SLAVE_ADDR 0x50  // 7-bit I2C address
 
+volatile unsigned char RXData[3];
+volatile unsigned char data_index = 0;
 
-volatile unsigned char RXData[4];  // Array to store secret code chars
-
-unsigned char *p_RXData = RXData;
-unsigned char RXCount = 0;
 void main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;      // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
 
-    // ----- Clock setup -----
-    BCSCTL1 = CALBC1_1MHZ;         // DCO = 1 MHz
-    DCOCTL  = CALDCO_1MHZ;
+    // Clock setup
+    BCSCTL1 = CALBC1_1MHZ;
+    DCOCTL = CALDCO_1MHZ;
 
-    // ----- Configure I2C pins -----
-    // P1.6 = SCL, P1.7 = SDA
-    P1SEL  |= BIT6 | BIT7;         // Select I2C function
+    // I2C Pins: P1.6 = SCL, P1.7 = SDA
+    P1SEL  |= BIT6 | BIT7;
     P1SEL2 |= BIT6 | BIT7;
-    P1DIR  &= ~(BIT6 | BIT7);      // Inputs (open-drain)
-    P1REN  &= ~(BIT6 | BIT7);      // Disable internal pull-ups/downs
-    P1OUT  &= ~(BIT6 | BIT7);
 
-    // ----- USCI_B0 I2C Master setup -----
-    UCB0CTL1 |= UCSWRST;           // Hold in reset during config
-    UCB0CTL0  = UCMST | UCMODE_3 | UCSYNC; // I2C master, synchronous mode
-    UCB0CTL1  = UCSSEL_2 | UCSWRST;        // SMCLK source
-    UCB0BR0   = 10;                // 1 MHz / 10 = 100 kHz SCL
-    UCB0BR1   = 0;
-    UCB0I2CSA = WRITE_ADDR;        // Set Slave address
-    UCB0CTL1 &= ~UCSWRST;          // Release USCI for operation
+    // USCI_B0 setup
+    UCB0CTL1 |= UCSWRST;                     // Enable SW reset
+    UCB0CTL0 = UCMST | UCMODE_3 | UCSYNC;    // I2C master, sync mode
+    UCB0CTL1 = UCSSEL_2 | UCSWRST;           // SMCLK
+    UCB0BR0 = 10;                            // 100kHz
+    UCB0BR1 = 0;
+    UCB0I2CSA = SLAVE_ADDR;                  // Slave Address
+    UCB0CTL1 &= ~UCSWRST;                    // Clear SW reset
+    IE2 |= UCB0RXIE;                         // Enable RX interrupt
 
-    // ----- Generate START and address -----
-    UCB0CTL1 |= UCTR;      // TX mode 
-    UCB0CTL1 |= UCTXSTT;    // START condition
-    while (UCB0CTL1 & UCTXSTT);    // Wait for START to finish (address sent)
+    data_index = 0;
 
-    // ----- Generate STOP immediately -----
-    UCB0CTL1 |= UCTXSTP;           // Send STOP
-    while (UCB0CTL1 & UCTXSTP);    // Wait for STOP complete
+    // Start read operation
+    UCB0CTL1 &= ~UCTR;   // Receiver mode
+    UCB0CTL1 |= UCTXSTT; // Send START
 
-  while (1)
-  {
-    while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
-    UCB0CTL1 |= UCTXSTT;                    // I2C start condition
-    while (UCB0CTL1 & UCTXSTT);             // Start condition sent?
-    UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
-    __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+    __bis_SR_register(CPUOFF + GIE); // Enter LPM0 w/ interrupts
 
-
-
-  }
+    // Done receiving
+    __no_operation(); // Breakpoint here to check RXData[]
 }
 
-// USCI_B0 Data ISR
+// USCI_B0 RX ISR
 #pragma vector = USCIAB0TX_VECTOR
 __interrupt void USCIAB0TX_ISR(void)
 {
-    if (RXCount)
-    {
-        *p_RXData++ = UCB0RXBUF;                       // Get RX data and ++ptr
-        --RXCount;
+  if (UCB0RXBUF != 0xFF)
+  {
+    RXData[data_index++] = UCB0RXBUF;
+  }
+    if (data_index > 2) {
+        UCB0CTL1 |= UCTXSTP;                  // Send STOP
+        IFG2 &= ~UCB0RXIFG;                   // Clear flag
+        __bic_SR_register_on_exit(CPUOFF);    // Wake up
     }
-    else  // End communication and disable USCI
-    {
-
-    }
-  __bic_SR_register_on_exit(CPUOFF);        // Exit LPM0
 }
