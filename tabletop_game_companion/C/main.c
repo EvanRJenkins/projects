c8a4f061e84939d7184d1f78c9bbefa1b45b2109
EvanRJenkins <JenkinsE@jbu.edu>
2025-12-02 10:27:34 -0600

    Added interrupt structure, began making button interrupt logic (working without debounce), switched pins in init.c, added interrupt enable logic to main, fixed FSM switch statement by putting it in a while(1) loop.

--- tabletop_game_companion/C/main.c
+++ tabletop_game_companion/C/main.c
@@ -1,6 +1,7 @@
 #include <msp430g2452.h>
 #include "_74AHC164.h"
 #include "init.h"
+
 /*
 FSM type definition
 */
@@ -27,20 +28,54 @@ int main(void) {
   SIPO_pin_init(BIT0, BIT2, BIT1);
   SIPO_shift(0x14);
   /*
+  Enable P2.1 interrupt
+  */
+  __bis_SR_register(GIE);
+  P2IES |= BIT1;  // Set edge select to high-low
+  P2IFG &= ~BIT1;  // Clear flag
+  P2IE |= BIT1;  // Enable interrupt for P2.1
+  /*
   FSM loop
   */
-  switch (system_state) {
-    case IDLE:  // LPM and wait
-      break;
-    case COMMAND:  // Jump to MENU, COUNT, or RANDOM depending on user input
-      break;
-    case MENU:  // Configure RANDOM number range, mode (COUNT or RANDOM, etc.)
-      break;
-    case COUNT:  // Increment counter and display for some time
-      break;
-    case RANDOM:  // Make "random" number in set range and display for some time
-      break;
+  while (1) {
+    switch (system_state) {
+      case IDLE:  // LPM and wait
+        break;
+      case COMMAND:  // Jump to MENU, COUNT, or RANDOM depending on user input
+        i += 0x02;
+        SIPO_shift(0x02);  // Testing interrupt
+        break;
+      case MENU:  // Configure RANDOM number range, mode (COUNT or RANDOM, etc.)
+       break;
+      case COUNT:  // Increment counter and display for some time
+        break;
+      case RANDOM:  // Make "random" number in set range and display for some time
+        break;
+   }
   }
   return 0;  // End of program
 }
-// End of file
+/*
+Interrupts
+*/
+
+#pragma vector = PORT2_VECTOR
+__interrupt void Port_2_ISR(void) {
+  if (P2IFG & BIT1) {  // If falling edge interrupt
+    if (P2IES & BIT1) {
+        // Switch this to start debounce timer
+      P2IES &= ~BIT1;  // Switch flag trigger to rising edge
+    }
+    else {  // If rising edge interrupt
+      system_state = COMMAND;  // Go to COMMAND state
+      P2IES |= BIT1;  // Switch trigger back
+    }
+    P2IFG &= ~BIT1;
+  }
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void Timer_A0_ISR(void) {
+  
+
+}
