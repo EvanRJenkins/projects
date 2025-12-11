40713c7c74b7ea81ac4a5d4e40b1f42fd10264f5
Evan Jenkins <JenkinsE@jbu.edu>
2025-12-10 20:48:12 -0600

    Remove unnecessary whitespace and comments

--- tabletop_game_companion/C/_74AHC164.h
+++ tabletop_game_companion/C/_74AHC164.h
@@ -2,29 +2,23 @@
 This file provides functions for the MSP430g series MCUs
 to interface with the 74AHC164 SIPO shift register IC.
 */
-
 #ifndef _74AHC164_H
 #define _74AHC164_H
-
 /*
 NOTE: These functions are designed for the following two
 configurations:
 1. DSA and DSB tied together
 2. DSB tied to VCC
 */
-
 /*
 Global 74AHC164 input pin registers.
 */
-
 extern volatile unsigned char *pDSA_REG;  // Serial data
 extern volatile unsigned char *pMR_N_REG;  // Active-low RST
 extern volatile unsigned char *pCP_REG;  // Clock pulse
-
 /*
 Global input pin masks
 */
-
 extern unsigned char  DSA_PIN;  // Serial data
 extern unsigned char  MR_N_PIN;  // Active-low RST
 extern unsigned char  CP_PIN;  // Clock pulse
@@ -42,6 +36,5 @@ void SIPO_reset();
 
 // Shift one byte over 8 clock cycles
 void SIPO_shift(unsigned char shift_byte);
-
 
 #endif
